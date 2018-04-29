e655fc219187daa682b4fd3335d790bfaa66b17e
Jitendra Soni <jitendrasoni24@gmail.com>
2018-04-29 15:38:10 +0100

    first commit

---
+++ hello-world/src/main/java/com/dtech/doc/HelloWorldDocker.java
@@ -0,0 +1,18 @@
+package com.dtech.doc;
+
+/**
+ * Hello world!
+ *
+ */
+public class HelloWorldDocker 
+{
+    public static void main( String[] args )
+    {
+        for(int i = 0; i<10; i++) {
+        	try {
+				System.out.println("docker hello world " + i);
+				Thread.sleep(1000);
+			} catch (Exception e) {}
+        }
+    }
+}
